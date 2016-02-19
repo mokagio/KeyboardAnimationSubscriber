9730116a67b17b06f6a5a7400c5649ade8596638
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-02-19 20:21:18 +1100

    (☞ﾟヮﾟ)☞KeyboardAnimationSubscriber - Template framework project

---
+++ KeyboardAnimationSubscriber/KeyboardAnimationSubscriber.h
@@ -0,0 +1,19 @@
+//
+//  KeyboardAnimationSubscriber.h
+//  KeyboardAnimationSubscriber
+//
+//  Created by Giovanni on 19/02/2016.
+//  Copyright © 2016 mokagio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeyboardAnimationSubscriber.
+FOUNDATION_EXPORT double KeyboardAnimationSubscriberVersionNumber;
+
+//! Project version string for KeyboardAnimationSubscriber.
+FOUNDATION_EXPORT const unsigned char KeyboardAnimationSubscriberVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeyboardAnimationSubscriber/PublicHeader.h>
+
+
