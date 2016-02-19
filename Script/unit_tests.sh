fde0612a306d5ecb872e0a1a237d070e1f98ca03
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-02-19 21:06:59 +1100

    Add script to run unit tests

---
+++ Script/unit_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+xcrun xcodebuild \
+  -project KeyboardAnimationSubscriber.xcodeproj \
+  -scheme KeyboardAnimationSubscriber \
+  -destination 'platform=iOS Simulator,name=iPhone 6,OS=latest' \
+  test \
+  | \
+  bundle exec xcpretty --test --color
